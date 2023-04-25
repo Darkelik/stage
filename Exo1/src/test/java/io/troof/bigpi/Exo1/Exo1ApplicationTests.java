194b778fecac68f5e7b3a5434b830edb2a47ad8c
Frédéric Vaz <fredericvaz2016@gmail.com>
2023-04-25 09:25:30 +0200

    Exo 1 : OpenAPI avec methode GET

---
+++ Exo1/src/test/java/io/troof/bigpi/Exo1/Exo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.troof.bigpi.Exo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
