6c8e197c331876771e7246886d01a3f8595ff954
Frédéric Vaz <fredericvaz2016@gmail.com>
2023-04-27 16:46:16 +0200

    it works!

--- hello-user/src/main/java/io/troof/bigpi/controller/UserController.java
+++ hello-user/src/main/java/io/troof/bigpi/controller/UserController.java
@@ -2,15 +2,18 @@ package io.troof.bigpi.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.troof.bigpi.model.User;
 
 @RestController
@@ -30,12 +33,13 @@ public class UserController {
 		return tempUsers;
 	}
 	
-	
+	/*
 	@RequestMapping(value = "/users",method = RequestMethod.GET,produces = "application/json")
 	public List<User> firstPage(){
 		return users;
 	}
-	/*
+	
+	
 	@RequestMapping(value = "/users",method = RequestMethod.GET,produces = "application/json")
 	public String firstPage(){
 		String hellos = "";
@@ -45,11 +49,30 @@ public class UserController {
 		return hellos;
 	}*/
 	
+	@RequestMapping(value = "/users", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<?> getUsers(@RequestParam(required = false) String id) {
+	    if (id != null) {
+	        // Chercher l'utilisateur correspondant
+	        Optional<User> user = users.stream().filter(u -> u.getId().equals(id)).findFirst();
+	        if (user.isPresent()) {
+	            String username = user.get().getUsername();
+	            String message = "Troof : say hello " + username;
+	            return ResponseEntity.ok(message);
+	        } else {
+	            return ResponseEntity.notFound().build();
+	        }
+	    } else {
+	        // Retourner tous les utilisateurs
+	        return ResponseEntity.ok(users);
+	    }
+	}
+
+	
 	@DeleteMapping(path = {"/{id}"})
 	public User delete(@PathVariable("id") String id) {
 		User deletedUser = null;
 		for (User user : users) {
-			if (user.getId() == id) {
+			if (user.getId().equals(id)) {
 				users.remove(user);
 				deletedUser = user;
 				break;
@@ -60,9 +83,9 @@ public class UserController {
 	
 	@PostMapping
 	public User create(@RequestBody User user) {
-		users.add(user);
-		System.out.println(user);
-		return user;
+	    users.add(user);
+	    System.out.println(user);
+	    return user;
 	}
-
+	
 }
--- hello-user/src/main/java/io/troof/bigpi/model/User.java
+++ hello-user/src/main/java/io/troof/bigpi/model/User.java
@@ -1,29 +1,29 @@
 package io.troof.bigpi.model;
 
 public class User {
-	private String id;
-	private String username;
-	
-	public User() {
-		
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	
-	public void setUsername(String username) {
-		this.username=username;
-	}
-	
-	public void setId(String id) {
-		this.id=id;
-	}
-	
+
+    private String id;
+    private String username;
+    
+    public User() {
+    	id=null;
+    	username=null;
+    }
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
