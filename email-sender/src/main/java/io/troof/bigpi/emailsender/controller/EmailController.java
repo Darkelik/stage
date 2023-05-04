f33422f48d5867dfdc8e21e03722b9f2e7600359
Frédéric Vaz <fredericvaz2016@gmail.com>
2023-05-04 10:57:03 +0200

    more junit tests

--- email-sender/src/main/java/io/troof/bigpi/emailsender/controller/EmailController.java
+++ email-sender/src/main/java/io/troof/bigpi/emailsender/controller/EmailController.java
@@ -2,6 +2,10 @@ package io.troof.bigpi.emailsender.controller;
 
 import io.troof.bigpi.emailsender.resource.EmailMessage;
 import io.troof.bigpi.emailsender.service.EmailSenderService;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,27 +17,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmailController {
 
+  List<EmailMessage> emails;
+  
   private final EmailSenderService emailSenderService;
 
   public EmailController(EmailSenderService emailSenderService) {
     this.emailSenderService = emailSenderService;
+    this.emails = new ArrayList<EmailMessage>();
   }
 
   /** Recevoir les informations. */
   @PostMapping("/send-email")
   public ResponseEntity<String> sendEmail(@RequestBody EmailMessage emailMessage) {
-    String to = emailMessage.getTo();
-    String cc = emailMessage.getCc();
-    String bcc = emailMessage.getBcc();
-    String subject = emailMessage.getSubject();
-    String message = emailMessage.getMessage();
+      
+    try {
+      String to = emailMessage.getTo();
+      
+      if (to == null || to == "") {
+        return ResponseEntity.badRequest().body("'To' cannot be empty");
+      }
+      
+      if (!EmailValidator.getInstance().isValid(to)) {
+        return ResponseEntity.badRequest().body("Invalid email address");
+      }
+      
+      String subject = emailMessage.getSubject();
+      String message = emailMessage.getMessage();
+      
+      if (subject == null || subject == "") {
+        return ResponseEntity.badRequest().body("'Subject' cannot be empty");
+      }
+      if (message == null || message == "") {
+        return ResponseEntity.badRequest().body("'Message' cannot be empty");
+      }
+    
+      String cc = emailMessage.getCc();
+      String bcc = emailMessage.getBcc();
 
-    this.emailSenderService.sendEmail(to, cc, bcc, subject, message);
-    return ResponseEntity.ok("Success sending");
+      this.emailSenderService.sendEmail(to, cc, bcc, subject, message);
+      this.emails.add(emailMessage);
+      return ResponseEntity.ok("Success sending");
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error sending email");
+    }
   }
 
   @RequestMapping(value = "/emails", method = RequestMethod.GET, produces = "application/json")
-  public ResponseEntity<EmailSenderService> getEmails() {
-    return ResponseEntity.ok(emailSenderService);
+  public ResponseEntity<List<EmailMessage>> getEmails() {
+    return ResponseEntity.ok(emails);
   }
 }
--- email-sender/src/test/java/io/troof/bigpi/emailsender/controller/EmailControllerTest.java
+++ email-sender/src/test/java/io/troof/bigpi/emailsender/controller/EmailControllerTest.java
@@ -1,11 +1,13 @@
 package io.troof.bigpi.emailsender.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import io.troof.bigpi.emailsender.resource.EmailMessage;
 import io.troof.bigpi.emailsender.service.EmailSenderService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -20,13 +22,98 @@ class EmailControllerTest {
   private EmailController emailController;
 
   @SuppressWarnings("deprecation")
+  @BeforeEach
   public void setUp() {
     MockitoAnnotations.initMocks(this);
     emailController = new EmailController(emailSenderService);
   }
+  
+  @Test
+  void testSendEmailInvalidEmail() {
+    String to = "invalid-email";
+    String cc = "testcc@example.com";
+    String bcc = "testbcc@example.com";
+    String subject = "Test Subject";
+    String message = "Test Message";
+    EmailMessage emailMessage = new EmailMessage(to, cc, bcc, subject, message);
+
+    ResponseEntity<String> response = emailController.sendEmail(emailMessage);
+
+    verify(emailSenderService, times(0)).sendEmail(to, cc, bcc, subject, message);
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals("Invalid email address", response.getBody());
+  }
+  
+  @Test
+  void testSendEmailEmptyEmail() {
+    String to = "";
+    String cc = "testcc@example.com";
+    String bcc = "testbcc@example.com";
+    String subject = "";
+    String message = "Test Message";
+    EmailMessage emailMessage = new EmailMessage(to, cc, bcc, subject, message);
+
+    ResponseEntity<String> response = emailController.sendEmail(emailMessage);
+
+    verify(emailSenderService, times(0)).sendEmail(to, cc, bcc, subject, message);
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals("'To' cannot be empty", response.getBody());
+  }
 
   @Test
-  void testSendEmail() {
+  void testSendEmailEmptySubject() {
+    String to = "test@example.com";
+    String cc = "testcc@example.com";
+    String bcc = "testbcc@example.com";
+    String subject = "";
+    String message = "Test Message";
+    EmailMessage emailMessage = new EmailMessage(to, cc, bcc, subject, message);
+
+    ResponseEntity<String> response = emailController.sendEmail(emailMessage);
+
+    verify(emailSenderService, times(0)).sendEmail(to, cc, bcc, subject, message);
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals("'Subject' cannot be empty", response.getBody());
+  }
+
+  @Test
+  void testSendEmailEmptyMessage() {
+    String to = "test@example.com";
+    String cc = "testcc@example.com";
+    String bcc = "testbcc@example.com";
+    String subject = "Test Subject";
+    String message = "";
+    EmailMessage emailMessage = new EmailMessage(to, cc, bcc, subject, message);
+
+    ResponseEntity<String> response = emailController.sendEmail(emailMessage);
+
+    verify(emailSenderService, times(0)).sendEmail(to, cc, bcc, subject, message);
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals("'Message' cannot be empty", response.getBody());
+  }
+
+  @Test
+  void testSendEmailServiceError() {
+    String to = "test@example.com";
+    String cc = "testcc@example.com";
+    String bcc = "testbcc@example.com";
+    String subject = "Test Subject";
+    String message = "Test Message";
+    EmailMessage emailMessage = new EmailMessage(to, cc, bcc, subject, message);
+
+    doThrow(new RuntimeException("Unable to send email"))
+      .when(emailSenderService)
+        .sendEmail(to, cc, bcc, subject, message);
+
+    ResponseEntity<String> response = emailController.sendEmail(emailMessage);
+
+    verify(emailSenderService, times(1)).sendEmail(to, cc, bcc, subject, message);
+    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    assertEquals("Error sending email", response.getBody());
+  }
+
+  @Test
+  void testSendEmailWithCcAndBcc() {
     String to = "test@example.com";
     String cc = "testcc@example.com";
     String bcc = "testbcc@example.com";
@@ -40,4 +127,21 @@ class EmailControllerTest {
     assertEquals(HttpStatus.OK, response.getStatusCode());
     assertEquals("Success sending", response.getBody());
   }
+
+  @Test
+  void testSendEmailWithoutCcAndBcc() {
+    String to = "test@example.com";
+    String cc = "testcc@example.com";
+    String bcc = "testbcc@example.com";
+    String subject = "Test Subject";
+    String message = "Test Message";
+    EmailMessage emailMessage = new EmailMessage(to, cc, bcc, subject, message);
+
+    ResponseEntity<String> response = emailController.sendEmail(emailMessage);
+
+    verify(emailSenderService, times(1)).sendEmail(to, cc, bcc, subject, message);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertEquals("Success sending", response.getBody());
+  }
+
 }
