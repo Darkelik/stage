0b81fd4a893ea4372ca1f7269c1c8a216b8e0c1e
Frédéric Vaz <fredericvaz2016@gmail.com>
2023-05-09 10:52:29 +0200

    fixed mySQL workbench connexion issues

--- email-sender-registration/src/main/java/io/troof/bigpi/emailsenderregistration/entity/ConfirmationToken.java
+++ email-sender-registration/src/main/java/io/troof/bigpi/emailsenderregistration/entity/ConfirmationToken.java
@@ -1,14 +1,16 @@
 package io.troof.bigpi.emailsenderregistration.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import java.sql.Date;
+import java.util.Date;
 
 /** Defining a confirmation token. */
+@Entity
 public class ConfirmationToken {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
--- email-sender-registration/src/main/java/io/troof/bigpi/emailsenderregistration/entity/User.java
+++ email-sender-registration/src/main/java/io/troof/bigpi/emailsenderregistration/entity/User.java
@@ -1,19 +1,31 @@
 package io.troof.bigpi.emailsenderregistration.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 
 /** Defining a user. */
+@Entity
 public class User {
   
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "user_id")
   private Long userId;
+  
+  @Column(name = "user_name")
   private String userName;
+  
+  @Column(name = "user_email")
   private String userEmail;
+  
+  @Column(name = "user_password")
   private String userPassword;
+  
+  @Column(name = "is_enabled")
   private boolean isEnabled;
   
   public User() {
